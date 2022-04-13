5f8c96138360bc6122f6c1579b0a902ac5c0971c
auoHan <710543268@qq.com>
2022-04-13 22:16:45 +0800

    初始化项目

---
+++ vite.config.ts
@@ -0,0 +1,56 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
+import viteCompression from 'vite-plugin-compression'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // gzip压缩 生产环境生成 .gz 文件
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  css: {
+    //css预处理
+    preprocessorOptions: {
+      scss: {
+        /*
+        引入var.scss全局预定义变量，
+        如果引入多个文件，
+        可以使用
+        '@import "@/assets/scss/globalVariable1.scss";@import "@/assets/scss/globalVariable2.scss";'
+        这种格式
+         */
+        additionalData: '@import "@/assets/scss/index.scss";'
+      }
+    }
+  },
+  //启动服务配置
+  server: {
+    host: '0.0.0.0',
+    port: 8000,
+    open: true,
+    https: false,
+    proxy: {}
+  },
+  // 生产环境打包配置
+  //去除 console debugger
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  }
+})
