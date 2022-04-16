5194877216e1df09b2a6429e72551c1992ab504f
auoHan <710543268@qq.com>
2022-04-16 19:32:23 +0800

    引入ColumnList组件，写入内容测试

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <ColumnList :list="list"></ColumnList>
 </template>
 
-<style>
+<script setup lang="ts">
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {reactive} from 'vue';
+import ColumnList, {ColumnProps} from '@/components/ColumnList.vue';
+const list = reactive<ColumnProps[]>([
+  {
+    id: 1,
+    title: '标题1',
+    avatar: 'https://s3.amazonaws.com/media-p.slid.es/uploads/320262/images/6946205/logo.png',
+    description: '描述1'
+  },
+  {
+    id: 2,
+    title: '标题2',
+    //avatar: 'https://s3.amazonaws.com/media-p.slid.es/uploads/320262/images/6946205/logo.png',
+    description: '描述2'
+  }
+])
+</script>
+
+<style lang="scss" scoped>
 </style>
