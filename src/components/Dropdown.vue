7d92153131d47085d2afc9193b85c57ab75513d7
auoHan <710543268@qq.com>
2022-04-18 00:07:30 +0800

    button替换为a，加入slot

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="dropdown">
-    <button class="btn btn-primary dropdown-toggle my-2" type="button" @click.prevent="toggleOpen">
+    <a class="btn btn-primary dropdown-toggle my-2" href="#" @click.prevent="toggleOpen">
       {{ title }}
-    </button>
+    </a>
 
     <ul v-if="isOpen" :style="{display: 'block'}" class="dropdown-menu">
-      <li>
-        <a class="dropdown-item" href="#">新建文章</a>
-      </li>
-      <li>
-        <a class="dropdown-item" href="#">编辑资料</a>
-      </li>
+      <slot></slot>
     </ul>
   </div>
 </template>
