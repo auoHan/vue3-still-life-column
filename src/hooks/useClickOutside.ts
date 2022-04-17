fc2de2e15812b341f28110753d1d834a0262e684
auoHan <710543268@qq.com>
2022-04-18 01:07:27 +0800

    自定义useClickOutside函数

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,17 @@
+import {onMounted, onUnmounted, Ref, ref} from 'vue'
+
+export const useClickOutside = (elementRef: Ref<null | HTMLElement>) => {
+  const isClickOutside = ref(false)
+  const handler = (e: MouseEvent) => {
+    if (elementRef.value) {
+      isClickOutside.value = !elementRef.value.contains(e.target as HTMLElement)
+    }
+  }
+  onMounted(() => {
+    document.addEventListener('click', handler)
+  })
+  onUnmounted(() => {
+    document.removeEventListener('click', handler)
+  })
+  return isClickOutside
+}
