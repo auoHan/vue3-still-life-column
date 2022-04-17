e7c941b4bdf48826b7a43f92664df192ee32d800
auoHan <710543268@qq.com>
2022-04-18 00:32:37 +0800

    点击组件外部区域自动隐藏组件

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dropdown">
+  <div ref="dropdownRef" class="dropdown">
     <a class="btn btn-primary dropdown-toggle my-2" href="#" @click.prevent="toggleOpen">
       {{ title }}
     </a>
@@ -11,13 +11,29 @@
 </template>
 
 <script lang='ts' setup>
-import {ref} from 'vue'
+import {onMounted, onUnmounted, ref} from 'vue'
 
 const {title} = defineProps<{ title: string }>()
 const isOpen = ref(false)
+const dropdownRef = ref<null | HTMLElement>(null)
 const toggleOpen = () => {
   isOpen.value = !isOpen.value
 }
+// 点击隐藏事件
+const handler = (e: MouseEvent) => {
+  if (dropdownRef.value) {
+    // contains判断传入的节点是否为该节点的后代节点
+    if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen.value) {
+      isOpen.value = false
+    }
+  }
+}
+onMounted(() => {
+  document.addEventListener('click', handler)
+})
+onUnmounted(() => {
+  document.removeEventListener('click', handler)
+})
 </script>
 
 <style lang="scss" scoped>
