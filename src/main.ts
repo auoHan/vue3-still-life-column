9aa0ca14480f2a83c4157aec2c2d88311ef7988c
auoHan <710543268@qq.com>
2022-04-17 14:52:22 +0800

    解决bootstrap使用css不提示的问题

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
-import router from '@/router';
-import { createPinia } from "pinia"
+import router from '@/router'
+import {createPinia} from 'pinia'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 const app = createApp(App)
 app.use(router)
