fe449f675cd95036269691bb1b93e281a797e718
auoHan <710543268@qq.com>
2022-04-18 00:06:11 +0800

    完成结构样式

--- src/components/DropdownItem.vue
+++ src/components/DropdownItem.vue
@@ -1,11 +1,20 @@
 <template>
-
+  <li :class="{'is-disabled': disabled}" class="dropdown-option">
+    <slot></slot>
+  </li>
 </template>
 
 <script lang='ts' setup>
-
+const {disabled = false} = defineProps<{ disabled: boolean }>()
+console.log(disabled)
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+.dropdown-option.is-disabled {
+  * {
+    color: #6c757d;
+    pointer-events: none;
+    background-color: transparent;
+  }
+}
 </style>
