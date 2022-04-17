92e559cfd28dec467bb51f23b4f14adda77763ad
auoHan <710543268@qq.com>
2022-04-18 01:26:04 +0800

    删除handler事件，引入自定义useClickOutside函数

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -11,7 +11,8 @@
 </template>
 
 <script lang='ts' setup>
-import {onMounted, onUnmounted, ref} from 'vue'
+import {ref, watch} from 'vue'
+import {useClickOutside} from '@/hooks/useClickOutside'
 
 const {title} = defineProps<{ title: string }>()
 const isOpen = ref(false)
@@ -19,21 +20,15 @@ const dropdownRef = ref<null | HTMLElement>(null)
 const toggleOpen = () => {
   isOpen.value = !isOpen.value
 }
-// 点击隐藏事件
-const handler = (e: MouseEvent) => {
-  if (dropdownRef.value) {
-    // contains判断传入的节点是否为该节点的后代节点
-    if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen.value) {
-      isOpen.value = false
-    }
+// 点击隐藏事件，加入hooks
+const isClickOutside = useClickOutside(dropdownRef)
+// 使用监听事件，解决渲染时只改变一次的问题
+watch(isClickOutside, () => {
+  if (isOpen.value && isClickOutside.value) {
+    isOpen.value = false
   }
-}
-onMounted(() => {
-  document.addEventListener('click', handler)
 })
-onUnmounted(() => {
-  document.removeEventListener('click', handler)
-})
+console.log(isOpen.value)
 </script>
 
 <style lang="scss" scoped>
