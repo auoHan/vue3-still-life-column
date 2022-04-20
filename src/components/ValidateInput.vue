f07df4fb5ee0eb11d1f092a26189a1c108e09704
auoHan <710543268@qq.com>
2022-04-20 16:52:12 +0800

    编写结构样式，使用抽象规则，支持v-model，使用$attrs支持默认属性

--- src/components/ValidateInput.vue
+++ src/components/ValidateInput.vue
@@ -1,11 +1,80 @@
 <template>
-
+  <div class="validate-input-container pb-3">
+    <input
+      v-model="newValue"
+      :class="{'is-invalid': error}"
+      class="form-control"
+      v-bind="$attrs"
+      @blur="validateInput">
+    <span v-if="error" class="invalid-feedback">
+      {{ message }}
+    </span>
+  </div>
 </template>
 
 <script lang='ts' setup>
+import {computed, reactive, toRefs} from 'vue'
+
+const emailReg = /^[a-zA-Z\d][\w-]*[a-zA-Z\d]@[a-zA-Z\d][\w-]*[a-zA-Z\d]\.[a-zA-Z]+[a-zA-Z]$/
+
+// const passwordReg = /^[a-zA-Z]w{5,17}$/
+interface RuleProp {
+  type: 'required' | 'email',
+  message: string
+}
+
+export type RulesProp = RuleProp[]
+
+// 获取父组件的参数
+const props = defineProps<{
+  rules: RulesProp,
+  modelValue: string
+}>()
+// 发送事件
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+const inputRef = reactive({
+  error: false,
+  message: ''
+})
+const newValue = computed({
+  get: () => props.modelValue,
+  set: (value) => {
+    emit('update:modelValue', value)
+  }
+})
+const {error, message} = toRefs(inputRef)
+// 表单校验
+const validateInput = () => {
+  if (props.rules) {
+    const allPassed = props.rules.every(rule => {
+      let passed = true
+      inputRef.message = rule.message
+      switch (rule.type) {
+        case 'required':
+          passed = newValue.value.trim() !== ''
+          break
+        case 'email':
+          passed = emailReg.test(newValue.value)
+          break
+        default:
+          break
+      }
+      return passed
+    })
+    inputRef.error = !allPassed
+  }
+}
 
 </script>
-
+<script lang="ts">
+// 使用一个简单的 <script> to declare options
+export default {
+  // 禁用 Attribute 继承
+  inheritAttrs: false
+}
+</script>
 <style lang="scss" scoped>
 
 </style>
