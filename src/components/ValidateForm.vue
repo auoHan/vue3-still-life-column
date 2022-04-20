54788ffabf132b7bec5be66612314629eae2135a
auoHan <710543268@qq.com>
2022-04-20 18:10:04 +0800

    使用插槽，发送事件

--- src/components/ValidateForm.vue
+++ src/components/ValidateForm.vue
@@ -1,9 +1,21 @@
 <template>
-
+  <form class="validate-form-container">
+    <slot></slot>
+    <div class="submit-area" @click.prevent="submitForm">
+      <slot name="submit">
+        <button class="btn btn-primary" type="submit">提交</button>
+      </slot>
+    </div>
+  </form>
 </template>
 
 <script lang='ts' setup>
-
+const emit = defineEmits<{
+  (e: 'submitForm', value: boolean): void
+}>()
+const submitForm = () => {
+  emit('submitForm', true)
+}
 </script>
 
 <style lang="scss" scoped>
