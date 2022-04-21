6c73cedeb80c18670557b659e51037c8b14e80b4
auoHan <710543268@qq.com>
2022-04-21 15:36:18 +0800

    创建Home页面，编写基本结构样式，结合数据测试

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home-page">
+    <section class="py-5 text-center container">
+      <div class="row py-lg-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+          <img alt="callout" class="w-50" src="../assets/svg/callout.svg"/>
+          <h2 class="fw-light">随心写作，自由表达</h2>
+          <p>
+            <a class="btn btn-primary my-2" href="#">开始写文章</a>
+          </p>
+        </div>
+      </div>
+    </section>
+    <h4 class="fw-bold text-center">发现精彩</h4>
+    <ColumnList :list="list"></ColumnList>
+    <button v-if="!isLastPage" class="btn btn-outline-primary d-grid gap-2 col-2 mx-auto mt-2 mb-5"
+            @click="loadMorePage">加载更多
+    </button>
+  </div>
+
+</template>
+
+<script lang='ts' setup>
+
+import ColumnList from '@/components/ColumnList.vue'
+import {ColumnProps, testData} from '@/utils/testData'
+
+const list = reactive<ColumnProps[]>(testData)
+const isLastPage = ref(false)
+const loadMorePage = () => {
+  console.log('loadMorePage')
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
