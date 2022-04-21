d9fa481cd6875b80bd50b9a973f8aaa014555bc1
auoHan <710543268@qq.com>
2022-04-21 10:48:41 +0800

    创建GlobalFooter组件

---
+++ src/components/GlobalFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="text-center py-4 text-secondary bg-light mt-6">
+    <small>
+      <ul class="list-inline mb-0">
+        <li class="list-inline-item">&copy; 2022 四季专栏</li>
+        <li class="list-inline-item">A</li>
+        <li class="list-inline-item">B</li>
+        <li class="list-inline-item">C</li>
+        <li class="list-inline-item">D</li>
+      </ul>
+    </small>
+  </footer>
+</template>
+
+<script lang='ts' setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
