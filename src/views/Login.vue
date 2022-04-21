dd1b29497d9adcfd12c72c90adcdd376f582d7b5
auoHan <710543268@qq.com>
2022-04-21 15:36:55 +0800

    创建Login页面，编写基本结构样式，结合数据测试

---
+++ src/views/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <ValidateForm @submitForm="submitForm">
+    <div class="mb-3">
+      <label class="form-label">邮箱地址：</label>
+      <ValidateInput v-model="emailValue" :rules="emailRules" placeholder="请输入邮箱地址" type="text"
+      ></ValidateInput>
+    </div>
+    <div class="mb-3">
+      <label class="form-label">密码：</label>
+      <ValidateInput v-model="passwordValue" :rules="passwordRules" placeholder="请输入密码"
+                     type="password"></ValidateInput>
+    </div>
+    <template #submit>
+      <button class="btn btn-primary mb-3 d-grid gap-2 col-1 mx-auto" type="submit">登录</button>
+    </template>
+  </ValidateForm>
+</template>
+
+<script lang='ts' setup>
+
+import ValidateInput, {RulesProp} from '@/components/ValidateInput.vue'
+import ValidateForm from '@/components/ValidateForm.vue'
+
+const emailRules: RulesProp = [
+  {type: 'required', message: '电子邮箱地址不能为空'},
+  {type: 'email', message: '请输入正确的电子邮箱格式'}
+]
+const passwordRules: RulesProp = [
+  {type: 'required', message: '密码不能为空'}
+]
+const emailValue = ref(null)
+const passwordValue = ref(null)
+const submitForm = (result: boolean) => {
+  emailValue.value = null
+  passwordValue.value = null
+  if (result) {
+    useRouter().push({name: 'ColumnDetail', params: {id: 1}})
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
