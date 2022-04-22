c6a025d5a4d485ea16b27854d68c504e97753828
auoHan <710543268@qq.com>
2022-04-22 17:21:04 +0800

    新增全局前置守卫，解决在router中使用pinia遇到的问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
-
+import {useColumnsStore} from '@/store/columns'
+//const {user} = storeToRefs(useColumnsStore()) // 写在这会报错，原因是pinia只能在运行时使用
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
@@ -30,6 +31,13 @@ const routes: RouteRecordRaw[] = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+})
+router.beforeEach((to, from) => {
+  // pinia调用需要写在这，使用时调用，这样pinia已经挂载在了全局，不然会显示未定义pinia
+  const {user} = storeToRefs(useColumnsStore())
+  if (to.name !== 'login' && !user.value.isLogin) {
+    return router.push('/login')
+  }
 })
 
 export default router
