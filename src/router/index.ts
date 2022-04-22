ea24f2b6e01f1f4655af127e1588b1acb355bb77
auoHan <710543268@qq.com>
2022-04-22 14:12:06 +0800

    新增跳转新建文章路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    redirect: '/login'
+    redirect: '/home'
   },
   {
     path: '/home',
@@ -18,7 +18,12 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/column/:id',
     name: 'column',
-    component: () => import('@/views/ColumnDetail.vue'), // 注意这里要带上 文件后缀.vue
+    component: () => import('@/views/ColumnDetail.vue') // 注意这里要带上 文件后缀.vue
+  },
+  {
+    path: '/create',
+    name: 'create',
+    component: () => import('@/views/CreatePosts.vue') // 注意这里要带上 文件后缀.vue
   },
 ]
 
